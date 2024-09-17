c61200da97d565f5f2fdffc05fb800658ab7d0f5
miette <leguyader.doriane@gmail.com>
2024-09-17 14:30:05 +0200

    initialisation projet

--- src/Controller/ConcertController.php
+++ src/Controller/ConcertController.php
@@ -2,17 +2,20 @@
 
 namespace App\Controller;
 
+use App\Entity\Concert;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class ConcertController extends AbstractController
 {
-    #[Route('/concert', name: 'app_concert')]
-    public function index(): Response
+    #[Route('/concert', name: 'create_concert')]
+    public function createDataConcert(EntityManagerInterface $entityManager): Response
     {
-        return $this->render('concert/index.html.twig', [
-            'controller_name' => 'ConcertController',
-        ]);
+        $concert = new Concert();
+        $concert->
+
     }
+    
 }
